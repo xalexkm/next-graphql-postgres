f223e37586a64b0c1a073059b272adce4a248332
xalexkm <xalexkm@gmail.com>
2024-01-27 08:14:52 +0000

    add frontend files

--- backend/app.ts
+++ backend/app.ts
@@ -1,17 +1,17 @@
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { querySchema, userSchema } from "./schemas/index.js";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { usersResolvers, queryResolvers } from "./resolvers/index.js";
+import {ApolloServer} from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone";
+import {querySchema, userSchema} from "./schemas/index.js";
+import {makeExecutableSchema} from "@graphql-tools/schema";
+import {queryResolvers} from "./resolvers/index.js";
 import merge from "lodash/merge.js";
 
 const schema = makeExecutableSchema({
-  typeDefs: [userSchema, querySchema],
-  resolvers: merge([queryResolvers]),
+    typeDefs: [userSchema, querySchema],
+    resolvers: merge([queryResolvers]),
 });
 
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({schema});
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+const {url} = await startStandaloneServer(server, {
+    listen: {port: 4000},
 });
