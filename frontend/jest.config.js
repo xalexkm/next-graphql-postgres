24afab73785b1287b253ff759ba5fb2820fba1a5
xalexkm <xalexkm@gmail.com>
2024-01-27 08:38:41 +0000

    fix(jest): add transformer for jsx in test

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,8 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "jsdom",
-    transform: {
-        ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
-    },
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
+    "^.+\\.[jt]sx?$": [
+      "ts-jest",
+      {
+        tsconfig: {
+          jsx: "react-jsx",
+        },
+      },
+    ],
+  },
 };
