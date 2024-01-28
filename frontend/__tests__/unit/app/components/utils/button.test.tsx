9c2dd2106a19030af3deb55aaa63b372f7b6ff7f
xalexkm <xalexkm@gmail.com>
2024-01-28 06:06:35 +0000

    feat(button): add ref functionality

--- frontend/__tests__/unit/app/components/utils/button.test.tsx
+++ frontend/__tests__/unit/app/components/utils/button.test.tsx
@@ -1,18 +1,19 @@
-import {Button} from "../../../../../src/app/components/utils/button";
-import {render, fireEvent, screen} from "@testing-library/react";
+import { Button } from "../../../../../src/app/components/utils/button";
+import { render, fireEvent, screen } from "@testing-library/react";
+import React, { RefObject } from "react";
 
 import "@testing-library/jest-dom";
-import {userEvent} from "@testing-library/user-event";
+import { userEvent } from "@testing-library/user-event";
 
 test("render button with name and click", async () => {
-    // Arrange
-    const clickFn = jest.fn();
-    render(<Button onClick={clickFn}>Button</Button>);
-    // Act
-    await screen.findByRole("button");
-    await userEvent.click(screen.getByText("Button"));
-    await userEvent.hover(screen.getByText("Button"));
-    // Assert
-    expect(clickFn).toHaveBeenCalledTimes(1);
-    expect(screen.getByRole("button")).toHaveTextContent("Button");
+  // Arrange
+  const clickFn = jest.fn();
+  render(<Button onClick={clickFn}>Button</Button>);
+  // Act
+  await screen.findByRole("button");
+  await userEvent.click(screen.getByText("Button"));
+  await userEvent.hover(screen.getByText("Button"));
+  // Assert
+  expect(clickFn).toHaveBeenCalledTimes(1);
+  expect(screen.getByRole("button")).toHaveTextContent("Button");
 });
--- frontend/src/app/components/utils/button.tsx
+++ frontend/src/app/components/utils/button.tsx
@@ -1,16 +1,50 @@
 "use client";
 import className from "./button.module.scss";
-import {forwardRef} from "react";
+import {
+  ForwardedRef,
+  forwardRef,
+  LegacyRef,
+  Ref,
+  RefObject,
+  useImperativeHandle,
+  useRef,
+} from "react";
 
 type ButtonProps = {
-    onClick?: () => any;
-    children?: string;
+  onClick?: () => any;
+  children?: string;
+};
+
+export type ButtonImperativeHandle = {
+  focus: () => void;
 };
 
 export const Button = forwardRef(
-    ({onClick = () => void 0, children = "Button"}: ButtonProps, ref) => (
-        <button className={className.button} onClick={() => onClick()}>
-            {children}
-        </button>
-    ),
+  (
+    { onClick = () => void 0, children = "Button" }: ButtonProps,
+    ref: ForwardedRef<ButtonImperativeHandle>,
+  ) => {
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    if (ref !== null && buttonRef.current !== null) {
+      useImperativeHandle<ButtonImperativeHandle, ButtonImperativeHandle>(
+        ref,
+        (): ButtonImperativeHandle => ({
+          focus() {
+            buttonRef.current!.focus();
+          },
+        }),
+        [],
+      );
+    }
+    return (
+      <button
+        ref={buttonRef}
+        className={className.button}
+        onClick={() => onClick()}
+      >
+        {children}
+      </button>
+    );
+  },
 );
