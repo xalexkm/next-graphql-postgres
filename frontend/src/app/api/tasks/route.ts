6d24f27788f42b81414ef122fb589478b3c1e8b6
xalexkm <xalexkm@gmail.com>
2024-02-02 10:23:48 +0000

    feat(api/tasks): add session checks

--- frontend/src/app/api/tasks/route.ts
+++ frontend/src/app/api/tasks/route.ts
@@ -1,64 +1,19 @@
-import { gql } from "@apollo/client";
 import client from "@/src/app/client";
-
-const GET_All_TASKS = gql`
-  query GetAllTasks {
-    tasks {
-      estimated_difficulty
-      estimated_time
-      name
-      task_id
-      user_id
-    }
-  }
-`;
-
-const GET_All_TASKS_BY_USER_ID = gql`
-  query GetAllTasksByUserId($userId: String) {
-    getAllTasksByUserId(userId: $userId) {
-      estimated_difficulty
-      estimated_time
-      name
-      task_id
-      user_id
-    }
-  }
-`;
-
-const ADD_TASK = gql`
-  mutation CreateTask($input: InputTask) {
-    createTask(input: $input) {
-      estimated_difficulty
-      estimated_time
-      name
-      task_id
-      user_id
-    }
-  }
-`;
-
-const UPDATE_TASK = gql`
-  mutation UpdateTask($input: UpdateTask) {
-    updateTask(input: $input) {
-      estimated_difficulty
-      estimated_time
-      name
-      task_id
-      user_id
-    }
-  }
-`;
-
-const DELETE_TASK = gql`
-  mutation DeleteTask($taskId: String) {
-    deleteTask(taskId: $taskId)
-  }
-`;
+import { authenticate } from "@/src/app/api/login/utils";
+import {
+  ADD_TASK,
+  DELETE_TASK,
+  GET_All_TASKS_BY_USER_ID,
+  UPDATE_TASK,
+} from "@/src/app/api/sql";
 
 export async function POST(request: Request) {
-  const res = await request.json();
-  const userId = res.user_id;
-  // Here should be user_id validation
+  const { user_id: userId } = authenticate(request) ?? { user_id: null };
+  if (!userId)
+    return new Response(JSON.stringify({ message: BAD_AUTH }), {
+      status: 401,
+    });
+
   const data = await client.query({
     query: GET_All_TASKS_BY_USER_ID,
     variables: { userId },
@@ -68,37 +23,67 @@ export async function POST(request: Request) {
 }
 
 export async function PUT(request: Request) {
+  const { user_id: userId } = authenticate(request) ?? { user_id: null };
+  if (!userId)
+    return new Response(JSON.stringify({ message: BAD_AUTH }), {
+      status: 401,
+    });
+
   const res = await request.json();
-  // Here should be user_id validation
+
   const data = await client.mutate({
     mutation: ADD_TASK,
     variables: {
-      input: res,
+      input: {
+        ...res,
+        user_id: userId,
+      },
     },
+    fetchPolicy: "no-cache",
   });
+
   return Response.json(data.data.createTask);
 }
 
 export async function PATCH(request: Request) {
+  const { user_id: userId } = authenticate(request) ?? { user_id: null };
+
+  if (!userId)
+    return new Response(JSON.stringify({ message: BAD_AUTH }), {
+      status: 401,
+    });
+
   const res = await request.json();
-  // Here should be user_id validation
+
   const data = await client.mutate({
     mutation: UPDATE_TASK,
     variables: {
-      input: res,
+      input: {
+        ...res,
+        user_id: userId,
+      },
     },
+    fetchPolicy: "no-cache",
   });
   return Response.json(data.data.updateTask);
 }
 
 export async function DELETE(request: Request) {
+  const { user_id: userId } = authenticate(request) ?? { user_id: null };
+  if (!userId)
+    return new Response(JSON.stringify({ message: BAD_AUTH }), {
+      status: 401,
+    });
+
   const res = await request.json();
-  // Here should be user_id validation
+
   const data = await client.mutate({
     mutation: DELETE_TASK,
     variables: {
+      userId: userId,
       taskId: res.task_id,
     },
+    fetchPolicy: "no-cache",
   });
   return Response.json(data.data.deleteTask);
 }
